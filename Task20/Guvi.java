9467cebd3ac05153e231085b3773e913b50616ec
cibhichakaravarthi <54166096+cibhichakaravarthi@users.noreply.github.com>
2024-05-31 15:29:53 +0530

    Update Guvi.java

--- Task20/Guvi.java
+++ Task20/Guvi.java
@@ -23,47 +23,47 @@ public class Guvi extends mainChorme{
 		WebElement mobile = chromeDriver.findElement(By.xpath("//input[@id='mobileNumber']"));
 		WebElement signup = chromeDriver.findElement(By.xpath("//a[@id='signup-btn']"));
 		username.sendKeys("Cibhi");
-        email.sendKeys("test123@test.com");
-        password.sendKeys("Test@1234");
-        mobile.sendKeys("9876543210");
-        signup.click();
-        
-        String title = "GUVI | Sign Up";
-        if(chromeDriver.getTitle().equals(title)) {
-            System.out.println("User register successfully");
-        }
-        else {
-            System.out.println("User register failed");
-        }
+        	email.sendKeys("test123@test.com");
+	        password.sendKeys("Test@1234");
+	        mobile.sendKeys("9876543210");
+	        signup.click();
+	        
+	        String title = "GUVI | Sign Up";
+	        if(chromeDriver.getTitle().equals(title)) {
+	            System.out.println("User register successfully");
+	        }
+	        else {
+	            System.out.println("User register failed");
+	        }
+			
+		}
+		//login
+		@Test
+		public void login() {
+			
+			//load url
+			chromeDriver.get("https://www.guvi.in");
+			
+			//click login
+			WebElement login = chromeDriver.findElement(By.xpath("//a[@href=\"/sign-in/\"]"));
+			login.click();
+			
+			//locate Xpath
+			WebElement emailLogin = chromeDriver.findElement(By.xpath("//input[@id='email']"));
+			WebElement passwordLogin = chromeDriver.findElement(By.xpath("//input[@id='password']"));
+			WebElement submit = chromeDriver.findElement(By.xpath("//a[@id='login-btn']"));
+			emailLogin.sendKeys("test123@test.com");
+	        passwordLogin.sendKeys("Test@1234");
+	        submit.click();
+	        
+	        String titleLogin = "GUVI | Login";
+	        if(chromeDriver.getTitle().equals(titleLogin)) {
+	            System.out.println("User login successfully");
+	        }
+	        else {
+	            System.out.println("User login failed");
+	        }
+		}
 		
-	}
-	//login
-	@Test
-	public void login() {
-		
-		//load url
-		chromeDriver.get("https://www.guvi.in");
-		
-		//click login
-		WebElement login = chromeDriver.findElement(By.xpath("//a[@href=\"/sign-in/\"]"));
-		login.click();
-		
-		//locate Xpath
-		WebElement emailLogin = chromeDriver.findElement(By.xpath("//input[@id='email']"));
-		WebElement passwordLogin = chromeDriver.findElement(By.xpath("//input[@id='password']"));
-		WebElement submit = chromeDriver.findElement(By.xpath("//a[@id='login-btn']"));
-		emailLogin.sendKeys("test123@test.com");
-        passwordLogin.sendKeys("Test@1234");
-        submit.click();
-        
-        String titleLogin = "GUVI | Login";
-        if(chromeDriver.getTitle().equals(titleLogin)) {
-            System.out.println("User login successfully");
-        }
-        else {
-            System.out.println("User login failed");
-        }
-	}
-	
 
 }
